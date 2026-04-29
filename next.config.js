285999d90f2e7e9523c8c1f1d8f0544b02c1f110
elisaevrardordo <elisa.e2004@gmail.com>
2026-04-29 21:37:53 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,3 +6,20 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
+const nextConfig = {
+  images: {
+    domains: ['res.cloudinary.com'],
+  },
+  async redirects() {
+    return [
+      { source: '/products/:path*', destination: '/oeuvres', permanent: true },
+      { source: '/collections/:path*', destination: '/oeuvres', permanent: true },
+      { source: '/blogs/:path*', destination: '/le-journal', permanent: true },
+      { source: '/pages/la-maison', destination: '/la-maison', permanent: true },
+      { source: '/pages/experiences', destination: '/experiences', permanent: true },
+    ]
+  },
+}
+
+module.exports = nextConfig
