258dea8e4ca6baa754d552ee2b263a9f67c4f4d4
elisaevrardordo <elisa.e2004@gmail.com>
2026-05-23 20:13:53 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,9 @@
 import HeroVideo from '@/components/HeroVideo'
-import Manifeste from '@/components/Manifeste'
-import ProductGrid from '@/components/ProductGrid'
-import DualImage from '@/components/DualImage'
-import InstagramGrid from '@/components/InstagramGrid'
 
 export default function HomePage() {
   return (
-      <main>
-            <HeroVideo />
-                  <Manifeste />
-                        <ProductGrid />
-                              <DualImage />
-                                    <InstagramGrid />
-                                        </main>
-                                          )
-                                          }
+    <main>
+      <HeroVideo />
+    </main>
+  )
+}
