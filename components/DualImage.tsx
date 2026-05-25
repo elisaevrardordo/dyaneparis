0622e847a0c2c4c137a100fd720d7a3e5b077a4f
elisaevrardordo <elisa.e2004@gmail.com>
2026-05-26 00:09:26 +0200

    Update DualImage.tsx

--- components/DualImage.tsx
+++ components/DualImage.tsx
@@ -91,7 +91,7 @@ export default function DualImage() {
 {current === 1 && (
     <div style={{ position: 'relative', width: '100%', height: '800px' }}>
         <Image
-            src="https://res.cloudinary.com/dazhkrimv/image/upload/v1779746699/Apre%CC%80s_les_vendanges_bordelaines_b3yov0.png"
+            src="https://res.cloudinary.com/dazhkrimv/image/upload/v1779746937/Apre%CC%80s_les_vendanges_bordelaines_1_by3wu8.png"
             alt="Distillation Dyane"
             fill
             sizes="100vw"
